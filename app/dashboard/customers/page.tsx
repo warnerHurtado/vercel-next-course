922848a094d39d1b5f6adc69c78f9506bfabc0b0
warnerHurtado <warner3898@gmail.com>
2023-11-17 09:52:54 -0600

    feat: populate DB

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,4 +1,18 @@
 
 export default function page() {
-    return <p>Customer</p>
+    return <div style={{border: '1px solid blue', height: '100%'}}>
+       <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+       <h1 style={{fontSize: '24px', color: 'red', fontWeight: 800}}>
+            WARNER HURTADO, SOFTWARE ENGINEER
+        </h1>
+       </div>
+        <br></br>
+        <hr></hr>
+        <div style={{ border: '1px solid red'}}>
+        <p>
+            Customer
+        </p>
+        
+        </div>
+        </div>
 }
